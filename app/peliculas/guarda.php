e079322703166bf0a7195db7096278ec89eadefd
David Hernandez <isc.david.hernandez@gmail.com>
2024-07-05 14:44:49 -0600

    Otra

--- app/peliculas/guarda.php
+++ app/peliculas/guarda.php
@@ -13,23 +13,19 @@ if ($conn->query($sql)){
     if($_FILES['poster']['error'] == UPLOAD_ERR_OK){
         $permitidos = array("image/jpeg","image/jpeg)");
         if(in_array($_FILES['poster']['type'],$permitidos)){
-
-        
-        $dir = "posters";
-
-        $info_img = pathinfo($_FILES['poster']['name']);
-        $info_img['extension'];
-        $poster = $dir . '/' . $id . '.jpg';
-
-        if(!file_exists($dir)){
-            mkdir($dir,0777);
+            $dir = "posters";
+            $info_img = pathinfo($_FILES['poster']['name']);
+            $info_img['extension'];
+            $poster = $dir . '/' . $id . '.jpg';
+            if(!file_exists($dir)){
+                mkdir($dir,0777);
+            }
+            if(!move_uploaded_file($_FILES['poster']['tmp_name'],$poster)){
+                $_SESSION['msg'] = "Error al guardar imagen";
+            }
+        }else{
+            $_SESSION['msg'] = "Formato de imagen no permitido";
         }
-        if(!move_uploaded_file($_FILES['poster']['tmp_name'],$poster)){
-            $_SESSION['msg'] = "Error al guardar imagen";
-        }
-    }else{
-        $_SESSION['msg'] = "Formato de imagen no permitido";
-    }
     }
 }
 
